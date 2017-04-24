5fc3f73559e805a1418a21df7e7cd5c02478bbfe
Patrick Williams <Patrick Williams>
2017-04-24 21:42:12 +0100

    Initial Code Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterializeModule } from "angular2-materialize";
 
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+    {path: 'splash', component: SplashComponent},
+    {path:"app", component: AppComponent},
+    {path:"**", component: PageNotFoundComponent},
+    {path:'', redirectTo:'/splash', pathMatch:'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),  
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
